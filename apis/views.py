87a25becd024114ca5d6065dda26a5e4c47a05d1
thisisziihee <iamthejiheee@naver.com>
2020-02-09 16:27:43 +0900

    BaseView

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
-# Create your views here.
+
+@method_decorator(csrf_exempt, name = "dispatch")
+class BaseView(View):
+    @staticmethod
+        # json 형식(key : value)의 response 만드는 것
+        # 이제 이 BaseView를 상속받으면서 json 형식으로 만들면 됨
+    def response(data = {}, message = '', status = 200):
+        result = {
+            'data' : data,
+            'message' : message,
+        }
+        return JsonResponse(result, status = status)
