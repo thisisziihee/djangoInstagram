43ae17d6ee00a0275120cb354328b29b212f8638
thisisziihee <iamthejiheee@naver.com>
2020-02-10 15:00:34 +0900

    Login Logout Function

--- apis/views.py
+++ apis/views.py
@@ -3,11 +3,12 @@ from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-
-
+    #UserCreateView
 from django.core.validators import validate_email, ValidationError
 from django.db import IntegrityError
 from django.contrib.auth.models import User
+    #UserLoginView
+from django.contrib.auth import authenticate, login, logout
 
 
 
@@ -24,13 +25,13 @@ class BaseView(View):
         return JsonResponse(result, status = status)
 
 
-class UserCreateView(View):
+class UserCreateView(BaseView):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        username = request.POST.gt('username', '')
+        username = request.POST.get('username', '')
         if not username:
             return self.response(message = '아이디를 입력하세요.', status = 400)
         
@@ -50,3 +51,28 @@ class UserCreateView(View):
             return self.response(message = "이미 존재하는 아이디입니다.", status = 400)
 
         return self.response({'user.id' : user.id})
+
+
+class UserLoginView(BaseView):
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message = "아이디를 입력하세요.", status = 400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message = "비밀번호를 입력하세요.", status = 400)
+        
+        user = authenticate(request, username = username, password = password)
+
+        if user is None:
+            return self.response(message = "입력정보를 확인하세요.", status = 400)
+        
+        login(request, user)
+        return self.response()
+
+
+class UserLogoutView(BaseView):
+    def get(self, request):
+        logout(request)
+        return self.response()
+
