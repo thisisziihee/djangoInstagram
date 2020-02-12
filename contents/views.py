9944b6adb8f006ad9fde05f6abbda73923527fb6
thisisziihee <iamthejiheee@naver.com>
2020-02-12 12:21:37 +0900

    create relationView and add HomeView

--- contents/views.py
+++ contents/views.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import Content, FollowRelation
+from django.db.models import Prefetch
 
 
 @method_decorator(login_required, name = "dispatch")
@@ -12,6 +13,14 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         user = self.request.user
+
+        followees = FollowRelation.objects.filter(follower = user).values_list('followee_id', flat = True)
+
+        lookup_user_ids = [user.id] + list(followees)
+
+        context['contents'] = Content.objects.select_related('user').prefetch_related('image_set').filter(
+            user__id__in = lookup_user_ids
+        )
 
         return context
 
@@ -31,6 +40,6 @@ class RelationView(TemplateView):
         except FollowRelation.DoesNotExist:
             pass 
 
-        context['followers'] = FollowRelation.objects.select_related('folloewer').filter(followee__in = [user])
+        context['followers'] = FollowRelation.objects.select_related('follower').filter(followee__in = [user])
         return context
 
